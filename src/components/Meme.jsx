306ea9569d75b6e4d506db5f5b14620028e98b51
Dynamic Tech <adekanbiemmanuel109@gmail.com>
2023-10-17 19:56:47 +0100

    main file was added

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -27,7 +27,7 @@ function Meme() {
         <input type="text" placeholder="Botttom text" />
         <button onClick={getUrl}>Get a new meme image 🖼</button>
         <img src={meme.randomImage} className="meme-container" />
-        <p>how are </p>
+        <p>how are you doing today</p>
       </div>
     </main>
   );
