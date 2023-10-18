7d0f0f2f6b6c3b86ee1c3a0771ed45baae165da2
Dynamic Tech <adekanbiemmanuel109@gmail.com>
2023-10-18 18:48:03 +0100

    updated code to take users inputs as meme text

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -10,28 +10,55 @@ function Meme() {
 
   const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
+  const [memeText, setMemeText] = React.useState({
+    topText: "",
+    bottomText: "",
+  });
+
   function getUrl() {
     const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     const url = memesArray[randomNumber].url;
+
     setMeme((prevMeme) => ({
       ...prevMeme,
       randomImage: url,
     }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setMemeText((prevMemeText) => ({
+      ...prevMemeText,
+      [name]: value,
+    }));
+  }
+
   return (
     <main>
       <div className="form">
-        <input type="text" placeholder="Top text" />
-        <input type="text" placeholder="Botttom text" />
+        <input
+          type="text"
+          name="topText"
+          value={memeText.topText}
+          placeholder="Top text"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="bottomText"
+          placeholder="Botttom text"
+          onChange={handleChange}
+          value={memeText.bottomText}
+        />
         <button onClick={getUrl}>Get a new meme image 🖼</button>
       </div>
 
       <div className="meme">
         <img src={meme.randomImage} className="meme-container" />
-        <h2 className="meme-text top">One does not simply</h2>
-        <h2 className="meme-text bottom">Walk into Mordor</h2>
+        <h2 className="meme-text top">{memeText.topText}</h2>
+        <h2 className="meme-text bottom">{memeText.bottomText}</h2>
       </div>
     </main>
   );
